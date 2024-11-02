8fdaa4fd4be8ad00e71c0127bb4db8799454af5c
Ines Ruiz <inesruizmontero@gmail.com>
2024-11-02 01:13:19 +0100

    empresas-tabla

--- resources/views/admin/companies.blade.php
+++ resources/views/admin/companies.blade.php
@@ -6,10 +6,10 @@
         <div class="sm:flex sm:items-center sm:justify-between">
             <div>
                 <div class="flex items-center gap-x-3">
-                    <h2 class="text-lg font-medium text-gray-800 dark:text-white">Empresas</h2>
+                    <h2 class="text-lg font-medium text-gray-800 dark:text-black">Total Empresas:</h2>
 
                     <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{{$totalCompanies}}
-                        registradas</span>
+                        Registradas</span>
                 </div>
 
             </div>
@@ -24,7 +24,7 @@
 
 
 
-                        <span class="ms-2">Crear empresa</span></a>
+                        <span class="ms-2">Nueva Empresa</span></a>
                 </button>
             </div>
         </div>
@@ -41,96 +41,80 @@
                     <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                             <thead class="bg-gray-50 dark:bg-gray-800">
-
                                 <tr>
-                                    <th scope="col" class="py-3.5 px-3 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <th scope="col" class="py-3.5 px-3 text-sm font-normal text-left rtl:text-right text-white">
                                         Nombre
                                     </th>
-                                    <th scope="col" class="py-3.5 px-3 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Direccion
+                                    <th scope="col" class="py-3.5 px-3 text-sm font-normal text-left rtl:text-right text-white">
+                                        Dirección
                                     </th>
-
-                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Telefono
+                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-white">
+                                        Teléfono
                                     </th>
-
-                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-white">
                                         Email
                                     </th>
-
-                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-white">
                                         Web
                                     </th>
-                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-white">
                                         Acerca de la empresa
                                     </th>
-                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Opciones</th>
-
+                                    <th scope="col" class="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-white">
+                                        Opciones
+                                    </th>
                                 </tr>
                             </thead>
-                            <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-
+                            <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900 text-white">
                                 @foreach($empresas as $empresa)
-
                                 <tr>
                                     <td class="px-3 py-4 text-sm font-medium whitespace-nowrap">
                                         <div>
-                                            <h2>{{$empresa->nombre}}</h2>
-
+                                            <h2 class="text-white">{{$empresa->nombre}}</h2>
                                         </div>
                                     </td>
                                     <td class="px-3 py-4 text-sm font-medium whitespace-nowrap">
                                         <div>
-                                            <p class="font-medium text-gray-800 dark:text-white ">
-                                                {{$empresa->direccion}}
-                                            </p>
-
+                                            <p class="text-white">{{$empresa->direccion}}</p>
                                         </div>
                                     </td>
                                     <td class="px-3 py-4 text-sm font-medium whitespace-nowrap">
                                         <div>
-                                            <p>{{$empresa->telefono}}</p>
-
+                                            <p class="text-white">{{$empresa->telefono}}</p>
                                         </div>
                                     </td>
                                     <td class="px-3 py-4 text-sm font-medium whitespace-nowrap">
                                         <div>
-                                            <p class="text-gray-500 dark:text-gray-400">
-                                                {{$empresa->email}}
-                                            </p>
+                                            <p class="text-white">{{$empresa->email}}</p>
                                         </div>
                                     </td>
                                     <td class="px-3 py-4 text-sm whitespace-nowrap">
                                         <div>
-                                            <p>{{$empresa->web}}</p>
+                                            <p class="text-white">{{$empresa->web}}</p>
                                         </div>
                                     </td>
                                     <td class="px-3 py-4 text-sm whitespace-nowrap">
                                         <ul class="dropdown ml-3">
-                                            <button type="button" class="dropdown-toggle px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
+                                            <button type="button" class="dropdown-toggle px-1 py-1 text-white transition-colors duration-200 rounded-lg hover:bg-gray-100">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                                                 </svg>
-
                                             </button>
                                             <ul class="bg-slate-200 dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md border border-gray-100 w-full max-w-[140px]">
-                                                <li class="text-wrap text-center">{{$empresa->informacionExtra}}</li>
+                                                <li class="text-wrap text-center text-black">{{$empresa->informacionExtra}}</li>
                                             </ul>
                                         </ul>
                                     </td>
-
                                     <td class="px-3 py-4 text-sm whitespace-nowrap">
                                         <div>
-                                            <a href="{{ route('admin.companyDelete', ['id' => $empresa]) }}" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-[#f84525] hover:bg-gray-50">Eliminar
-                                                Empresa</a>
+                                            <a href="{{ route('admin.companyDelete', ['id' => $empresa]) }}" class="flex items-center text-[13px] py-1.5 px-4 text-white hover:text-[#f84525] hover:bg-gray-50">Eliminar Empresa</a>
                                         </div>
                                     </td>
                                 </tr>
                                 @endforeach
-
                             </tbody>
                         </table>
+
                         {{$empresas->links()}}
                     </div>
                 </div>
