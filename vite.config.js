5e3340298fcc07451326eb3f6d3f735aeb16939f
Ines <inesruizmontero@gmail.com>
2024-12-07 08:55:00 +0100

    vite-confg-error

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/admin.js',
                 'resources/js/explora.js',
+                'resources/js/welcome.js',
             ],
             refresh: true,
         }),
